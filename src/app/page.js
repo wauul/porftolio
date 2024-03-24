771e9311710199720e09851c127e823d9ef18c55
Wael Fezari <wael.fezari@epitech.eu>
2024-03-24 14:55:09 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
-export default function Home() {
+export default function Index() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">

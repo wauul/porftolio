cb32771d9b41a00e2a27253385c0181734a06902
Wael Fezari <wael.fezari@epitech.eu>
2024-03-27 20:11:42 +0100

    Added speed insight and analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,23 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from './ThemeProvider';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
+    
     <html lang="en">
+      
         <body className={inter.className}>
+          <SpeedInsights/>
+          <Analytics/>
           {children}
           </body>
     </html>
+    
   );
 }
 
